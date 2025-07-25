b965eb769e9b4ec3e85b38305901b59019610f6c
Gerry-Migiro <gersonmigiro@gmail.com>
2025-07-25 13:00:51 +0300

    Updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './features/public/home/home.component';
-import { TicketFormComponent } from './features/public/ticket-form/ticket-form.component';
-
+    
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'submit-ticket', component: TicketFormComponent }
+  { path: '', pathMatch: 'full',
+    loadComponent: () => { return import('./features/public/home/home.component').then(m => m.HomeComponent)
+    }
+  },
+
+  { path: 'submit-ticket', pathMatch: 'full',
+    loadComponent: () => { return import('./features/public/ticket-form/ticket-form.component').then(m => m.TicketFormComponent)
+    }
+  }
 ];
